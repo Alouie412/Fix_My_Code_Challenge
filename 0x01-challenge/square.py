eda01f6be7c1a02cc2bdb4de7b543c1f722d3fce
Alouie412 <alouie412@gmail.com>
2020-05-20 21:48:34 +0000

    Here's a problem. Fix it. ~Holberton

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""
+Docstring
+"""
 
-class square():
-    
+
+class rect():
+
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ Documentation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,14 +21,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Documentation """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Documentation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = rect(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
